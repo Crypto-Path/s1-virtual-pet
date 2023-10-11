a32efccf7527d7a76b124c9fb9d3835c683d4d07
maxre24 <cyphe371@gmail.com>
2023-10-11 14:48:16 -0700

    asd

--- cam-v1/Runner.java
+++ cam-v1/Runner.java
@@ -1,6 +1,47 @@
+import javax.swing.JFrame;
+import javax.swing.JOptionPane;
+
 public class Runner {
   public static void main(String[] args) {
-    VirtualPet p = new VirtualPet();
-    p.exercise();
+    new Runner();
   }
+
+  enum activities {
+    exercise,
+    feed
+  }
+
+  public Runner() {
+    // Main
+    VirtualPet p = new VirtualPet();
+
+    // Tasks
+    p.exercise();
+    wait(1000);
+    p.feed();
+    wait(1000);
+    p.exercise();
+    String ans = getResponse("How was your day?");
+    System.out.println(ans);
+  }
+
+  public String getResponse(String prompt) {
+    String res = (String)JOptionPane.showInputDialog(
+      new JFrame(),
+      prompt,
+      "Response",
+      JOptionPane.PLAIN_MESSAGE
+    );
+    return res;
+  }
+
+  public void wait(int millis) {
+    try {
+      Thread.sleep(millis);
+    } catch (Exception e) {
+      // TODO: handle exception
+    }
+  }
+
+  
 }
